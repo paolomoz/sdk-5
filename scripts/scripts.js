2d27a3f4bb2023255f823e393e360ebc21e7a2fb
Karl Pauls <kpauls@adobe.com>
2025-11-06 17:28:18 +0100

    Comment out buildHeroBlock function call and add figma mcp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -66,7 +66,7 @@ function buildAutoBlocks(main) {
       });
     }
 
-    buildHeroBlock(main);
+    // buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
